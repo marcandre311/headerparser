6026e506c5fdf2d1a1bb24ab8bdae9bb76f8ad6e
Marc Andre <m.a.denault@gmail.com>
2016-11-02 23:50:08 +0000

    app is ready to deploy

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
-
+var useragent = require('useragent');
 /* GET home page. */
+router.get('/api/whoami', function(req, res, next) {
+    
+    function language (str){
+        var sol= str.split(",");
+        return sol[0];
+    }
+        function os (str){
+        var sol= str.split(" ");
+        return sol[1];
+    }
+    
+    var ip = req.headers['x-forwarded-for'];
+    var langStr = req.headers['accept-language'];
+    var lang= language(langStr);
+    var os = useragent.parse(req.headers['user-agent']).os.toString();
+    
+  console.log(os);
+  var data = {ipaddress: ip,
+              language: lang,
+              software: os};
+  res.json(data);
+});
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  
+
+  res.render("index");
 });
 
 module.exports = router;
